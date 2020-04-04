68a511dbd29ab969a409cd8d6d13883f91ba1420
Shalev Abargel <shalev25a@gmail.com>
2020-04-04 15:37:27 +0300

    Add files via upload

--- Joomla!/MainTrain.py
+++ Joomla!/MainTrain.py
@@ -1,13 +1,21 @@
 from JOOMLADetector import JOOMLADetector
+from MyDB import MyDB
 
 
 class Main(object):
     def main(self):
+        mydb = MyDB()
+        mydb.connect()
         urls = ['www.itwire.com', 'brw.nl', 'www.autocrescente.com', 'www.byronbay.com.au',
                 'www.kentatearchitect.com', 'www.timclarkedesign.com', 'www.bssbfurniture.com']
         for url in urls:
             detector = JOOMLADetector(url)
-            print(url + ': ' + str(detector.detect()))
+            elements = detector.detect()
+            print(url + ': ' + str(elements))
+            if elements[0] is True and elements[1] != 'version not found':
+                if mydb.select(url) is not None:
+                    mydb.insert_data(url, 'JOOMLA!', elements[1])
+        mydb.disconnect()
 
 
 main = Main()
